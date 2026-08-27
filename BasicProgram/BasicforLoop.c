746565a3431e87df657f837d1dade46a42ce2582
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-08-27 22:24:17 +0600

    comment out the code

--- BasicProgram/BasicforLoop.c
+++ BasicProgram/BasicforLoop.c
@@ -5,7 +5,7 @@ int main()
     for (int i = 0; i <= n; i++)
     {
         // printf("Hello Programmer !\n");
-        printf("Hello World !\n");
+        // printf("Hello World !\n");
     }
     return 0;
 }
