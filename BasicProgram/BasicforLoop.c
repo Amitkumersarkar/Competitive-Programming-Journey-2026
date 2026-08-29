efa468a434157226d4ec03b9dcbdb09847e59ea9
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-08-29 23:59:26 +0600

    basic code

--- BasicProgram/BasicforLoop.c
+++ BasicProgram/BasicforLoop.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    // int n = 10;
-    // for (int i = 0; i <= n; i++)
+    int n = 10;
+    for (int i = 0; i <= n; i++)
     {
         // printf("Hello Programmer !\n");
         // printf("Hello World !\n");
+        printf("I Can do it ");
     }
     return 0;
 }
